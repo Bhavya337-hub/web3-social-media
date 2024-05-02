ecafe8e9e23429c74ad8bda69eb54fa3f92da263
UtsavRaithatha <utsav.r@ahduni.edu.in>
2024-05-03 00:54:51 +0530

    Authentication Page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
-import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
 import Home from "./pages/Home/Home";
+import { Web3Provider } from "./Web3Context";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Web3Provider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Web3Provider>
   );
 }
 
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { USEeFF } from "react";
+
+import { useWeb3 } from "../../Web3Context";
 import Navbar from "../../components/Navbar/Navbar";
 import CreatePost from "../../components/CreatePost/CreatePost";
 import Post from "../../components/Post/Post";
+import FollowingList from "../../components/FollowList/FollowList";
+import WelcomePage from "../WelcomePage/WelcomePage";
 
 const Home = () => {
+  const { account, isConnected, connectToMetaMask, error } = useWeb3();
+
+  if (!isConnected) {
+    return <WelcomePage error={error} connectTo MetaMask={connectToMetaMask} />;
+  }
+
+  useEffect(() => {
+    // Update displayAccount when account changes
+    if (account) {
+      setDisplayAccount(
+        `${account.substring(0, 6)}...${account.substring(account.length - 4)}`
+      );
+    }
+  }, [account]);
+
   return (
     <div className="Home">
-      {/* Bootstrap layout with three sections first one being one fourth second one being half and third one being one fourth*/}
       <div className="container mt-4">
         <div className="row">
           <div className="col-md-3">
@@ -16,7 +34,21 @@ const Home = () => {
             <CreatePost />
             <Post />
           </div>
-          <div className="col-md-3">{/* <FollowersList /> */}</div>
+          <div className="col-md-3">
+            <button>
+              <div className="d-flex">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/2048px-MetaMask_Fox.svg.png"
+                  className="btn-login"
+                  alt="login-btn"
+                />
+                <p className="px-2">
+                  {account && account.slice(0, 6)}...{account.slice(-4)}
+                </p>
+              </div>
+            </button>
+            <FollowingList />
+          </div>
         </div>
       </div>
     </div>
